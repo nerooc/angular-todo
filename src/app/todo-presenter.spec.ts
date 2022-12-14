f57f8d9005cd349e7d5365169c42f2bb871170a4
nerooc <tomasz_gajda@outlook.com>
2022-12-14 17:47:43 +0100

    Initial version of the app

---
+++ src/app/todo-presenter.spec.ts
@@ -0,0 +1,25 @@
+import { TodoPresenter } from './todo-presenter';
+
+describe('TodoPresenter', () => {
+  it('should create an instance', () => {
+    expect(new TodoPresenter()).toBeTruthy();
+  });
+
+  it('should add item',()=>{
+    let presenter = new TodoPresenter();
+    presenter.addItem("test","test");
+    expect(presenter.Items.length).toEqual(4);
+  });
+
+  it('should remove item',()=>{
+    let presenter = new TodoPresenter();
+    presenter.removeItem(1);
+    expect(presenter.Items.length).toEqual(2);
+  });
+
+  it('should set completed',()=>{
+    let presenter = new TodoPresenter();
+    presenter.setCompleted(1,true);
+    expect(presenter.Items[0].completed).toEqual(true);
+  });
+});
